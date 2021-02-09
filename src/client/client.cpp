c40e19078b3a34f3558f9eb5918e3d19f0d46836
Manning Zhang <manningzh17@gmail.com>
2021-02-08 18:56:33 -0900

    Restructure files

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,1 +1,53 @@
-// add client code
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string input;
+	string output;
+	// do {
+	// 	cin >> input;
+	// 	if (input == "quit")
+	// 		return 0;
+	// 	if (input == "make_user")
+	// 		cout << input;
+	// 	switch (cmd) {
+	// 	// quit program
+	// 	case 'q':
+	// 		return 0;
+	// 	// make user
+	// 	case 'm':
+	// 		cout << "write username\n";
+	// 		cin >> tmp;
+	// 		make_usr(users, tmp);
+	// 		break;
+	// 	// update name
+	// 	case 'n':
+	// 		cout << "id and name\n";
+	// 		cin >> id >> tmp;
+	// 		update_usrname(users, id, tmp);
+	// 		break;
+	// 	// update xp
+	// 	case 'x':
+	// 		cout << "id and xp\n";
+	// 		cin >> id >> i;
+	// 		update_usr_xp(users, id, i);
+	// 		break;
+	// 	// print list of users
+	// 	case 'p':
+	// 		print_usrs(users);
+	// 		break;
+	// 	// make friend
+	// 	case 'f':
+	// 		cout << "user id and new friend id\n";
+	// 		cin >> id >> i;
+	// 		make_friend(users, id, i);
+	// 		break;
+	// 	// check friend
+	// 	case 'c':
+	// 		cout << "user id and friend id\n";
+	// 		cin >> id >> i;
+	// 		cout << check_friend(users, id, i) << "\n";
+	// 	}
+	// } while ();
