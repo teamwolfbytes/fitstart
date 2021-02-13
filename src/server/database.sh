95341179471b0217770a5e36c35ab3b4ff850b3a
Manning Zhang <manningzh17@gmail.com>
2021-02-12 19:48:41 -0900

    Add ability for persistent storage
    Wrote a shell script to get or put user info

--- src/server/database.sh
+++ src/server/database.sh
@@ -10,20 +10,8 @@ function get-user
 function put-user
 {
 	record=$(grep -Fn "$1" database | cut -d : -f 1)
-	sed -i "\$record /\$3/c\\$4" database
+	sed -i "$record s/.*/$2/" database
 }
 
-function option
-{
-	case $1 in
-		"get-user")
-			get-user $2
-			;;
-		"put-user")
-			put-user $2 $3
-			;;
-	esac
-}
-
-# format: command, username, replacer
-option $1 $2 $3
+read func username replacer
+$func $username $replacer
